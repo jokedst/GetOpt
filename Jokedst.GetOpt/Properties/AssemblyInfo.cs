5513abcec8537ed8d5cfea2a195c0dc4856e651e
jokedst <jokedst@gmail.com>
2013-11-01 11:57:09 +0100

    Initial commit

---
+++ Jokedst.GetOpt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jokedst.GetOpt")]
+[assembly: AssemblyDescription("GetOpt for C# for parsing command line arguments")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jokedst.GetOpt")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48186808-3de3-46cc-bf8a-2d2a6aa81221")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SampleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SampleApp
+{
+    using Jokedst.GetOpt;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var separator = "|";
+            var field = 0;
+            var verbose = false;
+            var numeric = false;
+            string file = null;
+
+            var opts = new GetOpt(new[] 
+            {
+                new CommandLineOption('s', "separator", "Field separator", ParameterType.String, o => separator = (string)o),
+                new CommandLineOption('v', "verbose", "Show more info about found files", ParameterType.None, o => verbose = true),
+                new CommandLineOption('n', "numeric", "sort numerically", ParameterType.None, o => numeric = true),
+                new CommandLineOption('f', "field", "Which field to sort by. Default = 0", ParameterType.Integer, o => field = (int)o),
+                new CommandLineOption("file name", ParameterType.String, o => file = (string)o, true), 
+            });
+
+            try
+            {
+                opts.ParseOptions(args);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return;
+            }
+        }
+    }
+}
