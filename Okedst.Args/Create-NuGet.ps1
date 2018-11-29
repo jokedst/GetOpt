f43b569df97ffcb435e2a10dcfa74d50dd7a6ef6
Joakim Wennergren <joakim.wennergren@neovici.se>
2018-11-29 23:52:30 +0100

    Ok, NOW Args NuGet works

--- Okedst.Args/Create-NuGet.ps1
+++ Okedst.Args/Create-NuGet.ps1
@@ -1,5 +1,6 @@
 md -Force .\nugetStaging\content
 (Get-Content Args.cs).replace('Okedst', '$rootnamespace$') | Set-Content .\nugetStaging\content\Args.cs.pp
-Compress-Archive -Path .\nugetStaging\* -DestinationPath ./Okedst.Args.nupkg.zip
-rm -ErrorAction Ignore .\Okedst.Args.0.1.2.nupkg
-Rename-Item .\Okedst.Args.nupkg.zip -NewName .\Okedst.Args.0.1.2.nupkg
+# Compress-Archive -Path .\nugetStaging\* -DestinationPath ./Okedst.Args.nupkg.zip
+# rm -ErrorAction Ignore .\Okedst.Args.0.1.2.nupkg
+# Rename-Item .\Okedst.Args.nupkg.zip -NewName .\Okedst.Args.0.1.2.nupkg
+nuget.exe  pack .\nugetStaging\Okedst.Args.nuspec -Version 0.1.3
